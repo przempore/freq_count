6807aea3818ed5accc57a702b4d317d655eef35b
Przemyslaw Porebski <przemyslaw.porebski@nokia.com>
2018-02-08 15:58:30 +0100

    rm useless files

--- main/freq_count/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/przemo/Stuff/freq_analizer/freq_count
-# Build directory: /home/przemo/Stuff/freq_analizer/main/freq_count
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
