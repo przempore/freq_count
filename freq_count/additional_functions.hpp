325bb1bcfb767d852ec47be596a77409b2fab78a
Przemek <przempore@gmail.com>
2018-02-12 17:36:17 +0100

    commented test was removed as redundant

--- freq_count/additional_functions.hpp
+++ freq_count/additional_functions.hpp
@@ -13,7 +13,7 @@ namespace functions
 {
 
 template<typename T>
-const auto sort_map(std::map<T, size_t> const& unsorted_map)
+auto sort_map(std::map<T, size_t> const& unsorted_map)
 {
     std::map<size_t, std::vector<T>, std::greater<size_t>> sorted_map;
 
@@ -22,11 +22,11 @@ const auto sort_map(std::map<T, size_t> const& unsorted_map)
         sorted_map[frq].emplace_back(str);
     }
 
-    return sorted_map;
+    return std::move(sorted_map);
 }
 
 template <typename T>
-std::ostream& operator<< (std::ostream& out, const std::vector<T>& v)
+std::ostream& operator<< (std::ostream& out, std::vector<T> const& v)
 {
     if (v.empty())
     {
